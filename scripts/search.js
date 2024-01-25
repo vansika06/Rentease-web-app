1988e10d9eb5173a782972347e39d234137ad86e
vansika06 <153821110+vansika06@users.noreply.github.com>
2024-01-25 12:19:20 +0530

    Update search.js

--- scripts/search.js
+++ scripts/search.js
@@ -11,7 +11,8 @@ console.log(searchResults);
 const Ref = collection(db, "itemlist");
 
 let results=[];
-let itemId
+let id=[];
+//let itemId
  try{
  const q = query(Ref, where("itemName", "==", searchResults));
  const querySnapshot = await getDocs(q);//retrieving all such queries
@@ -20,8 +21,9 @@ let itemId
  querySnapshot.forEach((doc) => {
 //   // doc.data() is never undefined for query doc snapshots
    console.log(doc.id, " => ", doc.data());
-   itemId=doc.id;
+   let itemId=doc.id;
    results.push(doc.data())
+     id.push(itemId);
 //
  });}
 // window.location.href=`search.html?search=${encodeURIComponent(JSON.stringify(search))}`})
@@ -36,12 +38,18 @@ if(results.length===0){
   h2.textContent="Sorry Items Not Found!!!";
 }
 const searchRow=document.querySelector("#search")
+let c=0;
+ 
 for(let i of results){
-  display(i);}
-function display(i){
+   let iid;
+  iid=id[c];
+  c++;
+  console.log(iid);
+  display(i,iid);}
+function display(i,iid){
     const itemContainer = document.createElement("div");
     console.log("created")
-    itemContainer.id = itemId;
+    itemContainer.id = iid;
     itemContainer.classList.add("col-md-3"); 
     itemContainer.classList.add("items");
     itemContainer.classList.add("text-center");
